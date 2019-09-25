543a65ac831fa134dca7d48879524e19b836c3cd
Jaeggler <gonzalo.santacruzm@gmail.com>
2019-09-25 10:52:45 -0500

    Push 0

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { LoginComponent } from './login/login.component';
+import { MenuComponent } from './menu/menu.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    LoginComponent,
+    MenuComponent
   ],
   imports: [
     BrowserModule
